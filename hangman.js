9c3f24fd49399a6f4da6471e00b95a7b6580fd9f
gdemianishim <georgedemian84@gmail.com>
2025-11-03 09:43:58 -0500

    catching up

--- hangman.js
+++ hangman.js
@@ -1,1 +1,31 @@
-han
+var POSSIBLE_WORDS = ["obdurate", "verisimilitude", "defenestrate", "obsiquious", "dissonant", "toady", "idempotent"];
+var word = "";
+var guesses = "";
+var MAX_GUESSES = 6;
+var guess_count = MAX_GUESSES;
+var gameOver = false; 
+
+
+function newGame() {
+    var randomIndex = parseInt(Math.random()) * POSSIBLE_WORD.length);
+    word = POSSIBLE_WORDS[randomIndex];
+    guesses = "";
+    guess_count = MAX_GUESSES;
+    gameOver = false
+    
+    updatePage();
+}
+
+function guessLetter() {
+    var input = document.getElementById("guess");
+    var letter = input.value; 
+    guesses += letter; 
+    upadtePage();
+
+}
+
+function upadtePage(){
+    var clueString = "";
+    for (var i = 0; i < word.length; i++);
+}
+
