b93cc8595340714f80597125b884af1c8a72c28b
gdemianishim <georgedemian84@gmail.com>
2025-11-03 11:25:34 -0500

    finished guess function and HTML and CSS

--- hangman.js
+++ hangman.js
@@ -3,11 +3,11 @@ var word = "";
 var guesses = "";
 var MAX_GUESSES = 6;
 var guess_count = MAX_GUESSES;
-var gameOver = false; 
+var gameOver = false;
 
 
 function newGame() {
-    var randomIndex = parseInt(Math.random()) * POSSIBLE_WORD.length);
+    var randomIndex = parseInt(Math.random() * POSSIBLE_WORD.length);
     word = POSSIBLE_WORDS[randomIndex];
     guesses = "";
     guess_count = MAX_GUESSES;
@@ -19,18 +19,30 @@ function guessLetter() {
     var input = document.getElementById("guess");
     var letter = input.value;
     input.value = "";
-    if (word==""|| guesses.indexOf(letter)>=0 || gameOver === true) 
-    guesses += letter; 
+    if (word === "" || guesses.indexOf(letter) >= 0 || gameOver === true) {
+        return;
+    }
+
+    if (word.indexOf(letter) < 0) {
+        guess_count--;
+    }
+    guesses += letter;
     upadtePage();
-    
-       return;
+
+
 
 }
-if (word.indexOf(letter) < 0){
-
-}
-function upadtePage(){
+function upadtePage() {
     var clueString = "";
-    for (var i = 0; i < word.length; i++);
+    for (var i = 0; i < word.length; i++) {
+        var currentLetter = word.charAt(i);
+        if (guesses.indexOf(currentLetter) >= 0) {
+            clueString += currentLetter + " ";
+        }
+        else{
+            clueString+="_ ";
+        }
+    }
 }
+
 
